1a408c434dc21c596fe6175aca2c364222f201eb
Ivo Iliev <133795870+elsifeither@users.noreply.github.com>
2023-11-06 20:31:10 +0200

    Add files via upload

---
+++ EncapsulationExercise/Box/Main.java
@@ -0,0 +1,24 @@
+package EncapsulationExercise.Box;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double length = Double.parseDouble(sc.nextLine());
+        double width = Double.parseDouble(sc.nextLine());
+        double height = Double.parseDouble(sc.nextLine());
+
+        try {
+            Box box = new Box(length,width,height);
+            System.out.println(box.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+    }
+}
